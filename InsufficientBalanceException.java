adc475456f284945a3a7a57a0c4ac44ab1acca3f
SiddeshUB <106797665+SiddeshUB@users.noreply.github.com>
2024-02-14 19:21:52 +0530

    Delete InsufficientBalanceException.java

--- InsufficientBalanceException.java
+++
@@ -1,7 +0,0 @@
-package com.learn.ExceptionHandling;
-public class InsufficientBalanceException extends Exception {
-	public InsufficientBalanceException() {
-		super("Enter the amount lesser than balance");
-		
-	}
-}
